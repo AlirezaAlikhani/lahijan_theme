45f74f9a2d3e6222d8f2b4b60df0b599082eb415
yasin taghavi <mrtqvi@yahoo.com>
2023-01-21 23:35:18 +0330

    apply configuration and add alpine library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '0rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '4rem',
+      },
+    },
+    extend: {
+      fontFamily : {
+        'vazir' :  ["vazirmatn"],
+        'shabnam' :  ["shabnam"]
+      }
+    },
   },
   plugins: [],
 }
